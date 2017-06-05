c27795e907cc0bb1b449986778b16dae8726df7c
satheesh kannan <smarteye9626@gmail.com>
2017-06-05 17:32:30 +0530

    Create sring.cpp

---
+++ sring.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a[10];
+int n;
+int i=0;
+cout<<"enter the string";
+cin>>a[10];
+n=strlen(a);
+for(i=0;i<n;i++)
+{cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+    cout<<a[n-i];
+}
+return 0;
+getch();
+}
