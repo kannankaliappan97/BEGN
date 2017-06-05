dbb6e9d0c27ef59d23cc0878b21186f9f66e50b2
satheesh kannan <smarteye9626@gmail.com>
2017-06-05 15:27:28 +0530

    Update and rename add.java to constanen.cpp

---
+++ constanen.cpp
@@ -0,0 +1,16 @@
+#include<iostrem.h>
+#include<conio.h>
+int main()
+{
+  char a;
+  cin>>a;
+  if(a=A|a=a|a=I|a=i|a=o|a=O|a=U|a=u|a=E|a=e)
+  {
+    cout<<"the element has vowel";
+  }
+  else
+  {
+    cout<<"element is not voewl";
+  }
+  return o;
+}
