50097fa76f89adeba06d839fd7b9c02de0de8082
satheesh kannan <smarteye9626@gmail.com>
2017-06-05 14:54:15 +0530

    Create one.cpp

---
+++ one.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conoi.h>
+void main()
+{
+int n;
+  cin>>n;
+  if(n>0)
+  {
+    cout<<"pasitive no";
+  }
+  if(n<0)
+  {
+    cout<<"negative";
+  }
+  if(n==0)
+  {cout<<"zero";
+  }
+getch(o)}
