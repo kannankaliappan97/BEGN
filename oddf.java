345facd28257722c47e12960072b83c699f2a70f
satheesh kannan <smarteye9626@gmail.com>
2017-06-08 12:28:26 +0530

    Create oddf.java

---
+++ oddf.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class oddf
+{
+    public static void main(String a[])
+    {
+        Scanner o=new Scanner(System.in);
+        int n=o.nextInt();
+        int k;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2!=0)
+            System.out.println(i);
+                }
+    
+    }
+}
+
