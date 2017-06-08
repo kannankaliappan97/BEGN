eaf9b38f1150b21e82c92a33944b9ae4593d9bb7
satheesh kannan <smarteye9626@gmail.com>
2017-06-08 12:24:32 +0530

    Create evenf.java

---
+++ evenf.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class evenf
+{
+    public static void main(String a[])
+    {
+        Scanner o=new Scanner(System.in);
+        int n=o.nextInt();
+        int k;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            System.out.println(i);
+                }
+    
+    }
+}
