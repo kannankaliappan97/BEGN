6871dfcd80d4e32065563633e01363ca5f93c3ef
satheesh kannan <smarteye9626@gmail.com>
2017-07-03 15:28:24 +0530

    Create exit.java

---
+++ exit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class exit
+{
+    public static void main(String arg[])
+    {
+        Scanner ob=new Scanner(System.in);
+        char ch='Q';
+        switch(ch)
+        {
+            case 'Q':
+            System.out.println("Exit");
+            break;
+        }
+        
+    }
+}
