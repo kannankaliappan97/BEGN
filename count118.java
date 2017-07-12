4a5ad35ec7a887726c5a4bfca2bc2a327cf02967
satheesh kannan <smarteye9626@gmail.com>
2017-07-12 16:13:29 +0530

    Create count118.java

---
+++ count118.java
@@ -0,0 +1,24 @@
+import java.util.*;
+    public class count118 
+
+    {
+
+         public static void main(String[] args) 
+
+         {
+
+            int i = 0;
+
+            if(System.out.printf("hello world ") == null) {}
+
+            for(i = 1; i < 2; System.out.println("Hello World."))
+
+            {
+
+                i++;
+
+         	}
+
+         }
+
+    }
