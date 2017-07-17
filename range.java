3483b591c0e993f943c433b9b773559ba176728c
satheesh kannan <smarteye9626@gmail.com>
2017-07-17 14:38:41 +0530

    Create range.java

---
+++ range.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class range
+{
+    public static void main(String arg[])
+    {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        if(n>0&&n<10)
+        {
+         System.out.println(n);
+        }
+        else
+        {
+         System.out.println("Inavalid data");
+        }
+    }
+}
