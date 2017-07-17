38e649948c1648347473e54e100fad8a2d330faf
satheesh kannan <smarteye9626@gmail.com>
2017-07-17 15:41:05 +0530

    Create mul12.java

---
+++ mul12.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class mul12
+{
+    public static void main(String arg[])
+    {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int n1=ob.nextInt();
+        if(n>n1)
+        {
+            System.out.print(n1);
+        }
+        else
+        {
+            System.out.print("not valid");
+        }
+    }
+}
