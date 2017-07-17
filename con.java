8995ee577e271df90b41489dee7b5ab46e0721e5
satheesh kannan <smarteye9626@gmail.com>
2017-07-17 15:10:47 +0530

    Create con.java

---
+++ con.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Con{
+	public static void main(String[] args)
+	{
+	    Scanner ob=new Scanner(System.in);
+		String s1=ob.nextLine();
+		String s2=ob.nextLine();
+		System.out.println(s1+ s2);
+
+	}
+
+}
