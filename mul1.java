f3ca12a33a81d57ca0a226f609914abf58434d55
satheesh kannan <smarteye9626@gmail.com>
2017-07-17 15:27:33 +0530

    Create mul1.java

---
+++ mul1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class mul1
+{
+    public static void main(String arg[])
+    {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        for(int i=1;i<n;i++)
+        {
+            System.out.println(i*n);
+        }
+    }
+}
