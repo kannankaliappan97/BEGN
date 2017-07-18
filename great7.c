f6088cf198e1144d508a5a11781a4ba892d11f26
satheesh kannan <smarteye9626@gmail.com>
2017-07-18 15:33:50 +0530

    Create great7.c

---
+++ great7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i=0,j=0;
+
+    
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
