3c4a69544a0f5a51db21904aa109b6b58175982d
satheesh kannan <smarteye9626@gmail.com>
2017-07-25 02:05:26 -0700

    Create tod1.c

---
+++ tod1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n, n1;
+   
+    scanf("%d",&n);
+ 
+    scanf("%d",&n1);
+    for(int i=0;i<n;i++)
+    {
+        n1++;
+    }
+    printf("%d",n1);
+}
